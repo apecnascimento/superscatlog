b7277b41b5a2f5716068e333b61b4dddd8e849d1
pedro.nascimento <pedro.nascimento@clinicasim.com>
2020-08-08 19:10:47 -0300

    Add api basic structure

--- src/api/api.go
+++ src/api/api.go
@@ -1,2 +1,18 @@
 package api
 
+import (
+	"github.com/labstack/echo/v4"
+)
+
+// Routes return api pipeline
+func Routes() (e *echo.Echo) {
+
+	pipeline := echo.New()
+
+	RegisterMiddlewares(pipeline)
+
+	RegisterRoutes(pipeline)
+
+	return pipeline
+
+}
